42e2f23b765869a23febcca9fef6c9cb6e859650
kld <kld0093@hotmail.com>
2018-12-31 22:32:51 +0300

    Added GridBuilder

---
+++ Puzzle/NeighborGridBuilder.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace KLD.Core.Puzzle
+{
+    public class NeighborGridBuilder : MonoBehaviour
+    {
+        public int Width;
+        public int Height;
+
+        public float TileWidth = 1;
+        public float TileHeight = 1;
+
+        public Transform TilesParent;
+
+        public GameObject TilePrefab;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            NeighbotGrid<Tile> gridBuilder = new NeighbotGrid<Tile>(Width, Height, (x, y) =>
+            {
+                var tile = Instantiate(TilePrefab, TilesParent?? transform).AddComponent<Tile>();
+
+                tile.name = $"{x} - {y}";
+
+                tile.transform.localPosition = new Vector3((x - ((float)Width / 2) + TileWidth), (y - ((float)Height / 2) + TileHeight), 0);
+
+                return tile;
+            });
+        }
+    }
+
+}
